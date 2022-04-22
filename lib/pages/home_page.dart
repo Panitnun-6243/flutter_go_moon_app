7cca18006b2d8c1250e393ad772f34fca08ee59d
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-04-23 03:49:34 +0700

    Add Text header and Add Dropdown menu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deiceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deiceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.contain,
-            image: AssetImage("assets/images/astro_moon.png"),
+      body: SafeArea(
+        child: Container(
+          color: Colors.lightBlue,
+          child: _destinationDropDownWidget(),
+          width: _deviceWidth,
+          height: _deiceHeight,
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.05,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    List<DropdownMenuItem<String>> _items =
+        ['Tanny Station', 'Panitnun Station'].map(
+      (e) {
+        return DropdownMenuItem(
+          child: Text(e),
+          value: e,
+        );
+      },
+    ).toList();
+    return Container(
+      child: DropdownButton(
+        items: _items,
+        onChanged: (_) {},
       ),
     );
   }
